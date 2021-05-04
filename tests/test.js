5574d50b5a25bb38561c3ef736e955e6f7aeec4f
José Cortez <jcortezb@everis.com>
2021-05-04 07:45:52 -0500

    added test example

--- tests/test.js
+++ tests/test.js
@@ -1,12 +1,12 @@
-// var assert = require('assert');
+var assert = require('assert');
 
-// describe('String', function () {
-//     describe('#indexOf', function () {
-//         it('should return -1 when the substring is not present', function () {
-//             var myString = 'test';
+describe('String', function () {
+    describe('#indexOf', function () {
+        it('should return -1 when the substring is not present', function () {
+            var myString = 'test';
 
-//             assert.equal(-1, myString.indexOf('x'));
-//             assert.equal(-1, myString.indexOf('y'));
-//         });
-//     });
-// });
+            assert.equal(-1, myString.indexOf('x'));
+            assert.equal(-1, myString.indexOf('y'));
+        });
+    });
+});
