02a9c7e3326a723b4c7a76e00be81fc735a31729
José Cortez <jcortezb@everis.com>
2021-05-05 08:44:22 -0500

    intall depends test and first test get all /devops

--- tests/test.js
+++ tests/test.js
@@ -1,12 +1,37 @@
-var assert = require('assert');
 
-describe('String', function () {
-    describe('#indexOf', function () {
-        it('should return -1 when the substring is not present', function () {
-            var myString = 'test';
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+const expect = require('chai').expect;
 
-            assert.equal(-1, myString.indexOf('x'));
-            assert.equal(-1, myString.indexOf('y'));
-        });
+
+chai.use(chaiHttp);
+const url= 'http://localhost:3000';
+
+
+describe('get all devops msj: ',()=>{
+    it('should get all devops msj ', (done) => {
+    chai.request(url)
+    .get('/devops')
+    .end( function(err,res){
+    console.log(res.body)
+    expect(res).to.have.status(200);
+    done();
     });
-});
+    });
+   });
+   
+
+// describe('get devops msj with id 1: ',()=>{
+//     it('should get the devops msj with id 1', (done) => {
+//     chai.request(url)
+//     .get('/devops/1')
+//     .end( function(err,res){
+//     console.log(res.body)
+//     expect(res.body).to.have.property('id').to.be.equal(1);
+//     expect(res).to.have.status(200);
+//     done();
+//     });
+//     });
+//    });
+   
+   
